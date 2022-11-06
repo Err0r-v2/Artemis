a44f1baeafea51132e6c688c34b75b55237f38fb
stan <paquinstanislas@gmail.com>
2022-11-06 21:13:51 +0100

    First commit

---
+++ functions.py
@@ -0,0 +1,195 @@
+import wget
+import urllib.error
+import glob
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import random
+import time
+import requests
+from bs4 import BeautifulSoup
+import sys
+import os, platform
+import threading
+import math
+from colorama import Fore, Style
+
+
+def clear():
+   if platform.system() == 'Windows':
+      os.system('cls')
+   else:
+      os.system('clear')
+
+def truncate(number, digits) -> float:
+    # Improve accuracy with floating point operations, to avoid truncate(16.4, 2) = 16.39 or truncate(-1.13, 2) = -1.12
+    nbDecimals = len(str(number).split('.')[1])
+    if nbDecimals <= digits:
+        return number
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
+
+# Utilise Selenium pour résoudre le captcha de dl-protect.info (possible que ça ne fonctionne pas), bypass primaire
+# si la sécurité est pas ouf
+def bypass(url):
+    os.environ['WDM_LOG_LEVEL'] = '0'
+    user_agent = f'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/{random.randint(50, 99)}.0.3112.50 Safari/537.36'
+
+    options = Options()
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument("disable-notifications")
+    options.add_experimental_option("useAutomationExtension", False)  # Adding Argument to Not Use Automation Extension
+    options.headless = True
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get(url)
+
+    time.sleep(random.randint(1, 2))
+    try:
+        captcha_button = driver.find_element(by=By.CLASS_NAME, value='g-recaptcha')
+        driver.execute_script("arguments[0].click();", captcha_button)
+    except Exception as e:
+        print("Le lien n'est plus fonctionel ", e)
+        sys.exit(0)
+
+    time.sleep(1)
+
+    try:
+        return driver.find_element(by=By.PARTIAL_LINK_TEXT, value='https://1fichier.com').text
+
+    except Exception as e:
+        return input("Veuillez entrer l'url manuellement : ")
+
+
+def create_proxies(timeout):
+    print('Obtention des proxies...', end=' ')
+    start = time.time()
+    params = {'request': 'displayproxies',
+              'protocol': 'socks5',
+              'timeout': 2000,
+              'country': 'all',
+              'ssl': 'yes',
+              'anonymity': 'elite,anonymous'}
+
+    r = requests.get('https://api.proxyscrape.com/v2', params=params)
+
+    proxy_list = r.text.splitlines()
+    valid_proxies = []
+    print('Effectué.')
+    print('Test des proxies avec ipify et 1fichier...')
+
+    def test_proxy(proxy, url, timeout, valid_proxies_list: list):
+        proxies = {
+            'https': f'socks5h://{proxy}'
+        }
+
+        try:
+            requests.get('https://api.ipify.org', proxies=proxies, timeout=timeout)
+            try:
+                r = requests.get(url, proxies=proxies, timeout=timeout)
+
+                if r.status_code == 200:
+                    valid_proxies_list.append(proxy)
+            except:
+                pass
+
+        except:
+            pass
+
+    threads = []
+    for proxy in proxy_list:
+        thread = threading.Thread(target=test_proxy, args=(proxy, 'https://1fichier.com/', timeout, valid_proxies,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    print(f'Effectué en {truncate(time.time() - start, 3)} secondes. Nous avons {len(valid_proxies)} proxies prometteurs.')
+    time.sleep(3)
+    return valid_proxies
+
+
+# This function returns the attributes of a film by its zone telechargement url
+# return attr = [quality, language, size, 1fichier link]
+def get_attrs(film_url):
+    attrs = []
+    r_film = requests.get(film_url)
+    s_film = BeautifulSoup(r_film.text, 'html.parser')
+
+    for u_soup in s_film.find_all('strong'):
+        if u_soup.get_text() == 'Qualité :':
+            attrs.append(u_soup.next_sibling)
+        elif u_soup.get_text() == 'Langue :':
+            attrs.append(u_soup.next_sibling)
+        elif u_soup.get_text() == 'Taille du fichier :':
+            attrs.append(u_soup.next_sibling)
+
+    attrs.append(s_film.find(attrs={'rel': 'external nofollow'}).get('href'))
+
+    return attrs
+
+
+def bar_progress(current, total, width=80):
+    progress_message = "Téléchargement : %d%% [%s / %s]" % (
+        current / total * 100, convert_size(current), convert_size(total))
+    # Don't use print() as it will print in new line every time.
+    sys.stdout.write("\r" + progress_message)
+    sys.stdout.flush()
+
+
+# Convertis des bytes en une valeur + adaptée
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
+
+
+def dl(url, name):
+    try:
+        wget.download(url, bar=bar_progress, out='Films/')
+        list_of_files = glob.glob('Films/*')
+        latest_file = max(list_of_files, key=os.path.getctime)
+        os.rename(latest_file, f'Films/{name}.{latest_file[-3:]}')
+
+    except urllib.error.HTTPError:
+        print("Lien expiré veuillez réessayer plus tard")
+
+    except ConnectionAbortedError:
+        print("Téléchargement avorté... Veuillez relancer le téléchargement")
+
+    except KeyboardInterrupt:
+        print('\nArrêt du téléchargement...')
+        print('À bientôt!')
+
+
+def bypass_1fichier(url, proxies):
+    headers_form = {
+        'cookie': 'AF=22123'
+    }
+    data_form = {
+        'dl_no_ssl': 'yes',
+        'dlinline': 'off'
+    }
+    for proxy in proxies:
+        p = {
+            'https': f'socks5h://{proxy}',
+            'http': f'socks5h://{proxy}'
+        }
+        try:
+            r_form = requests.post(url, proxies=p, headers=headers_form, data=data_form)
+            soup_form = BeautifulSoup(r_form.text, 'html.parser')
+            print(Fore.GREEN+ 'Bypass effectué !'+ Style.RESET_ALL)
+            return soup_form.find(class_="ok btn-general btn-orange")['href']
+
+        except Exception as e:
+            print(e)
+
+        print("Le bypass n'a pas pu être effectué, peut-être qu'augmenter le timeout résoudra le problème...")
---
+++ main.py
@@ -0,0 +1,131 @@
+# coding=utf-8
+
+
+from functions import *
+
+
+# Fonction principale
+def main():
+    global film_link
+    global qualities
+
+    clear()
+    timeout = int(input('Veuillez entrer le timeout pratique pour les proxies : '))
+
+    proxies = create_proxies(timeout)
+    # Setting some variables
+    film_list = []
+    # The list that will contain all the qualities' link orf the choosen film
+    qualities = []
+
+    # Getting the film's name and entering the parameters in the url
+    clear()
+    search = input("Veuillez rentrer le nom du film : ")
+    payload = {'search': search, 'p': 'films'}
+    url = "https://www.zone-telechargement.onl/"
+
+    # Executing url
+    r = requests.get(url, params=payload)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # If not found then...
+    if "Aucune fiches trouvées." in soup.prettify():
+        print(Fore.RED + 'Pas de film trouvé' + Style.RESET_ALL)
+        sys.exit()
+
+    # Else print the film's list
+    else:
+        for u_soup in soup.find_all(class_="cover_infos_title"):
+            film_name = u_soup.find('a').get_text()
+            if film_name not in film_list:
+                film_list.append(u_soup.find('a').get_text())
+
+    a = 1
+    for film in film_list:
+        print(f'{a}. {film}')
+        a += 1
+
+    arg_film = int(input('Veuillez rentrer le nombre du film choisi : '))
+
+    film_name1 = film_list[arg_film - 1]
+
+    if arg_film > len(film_list) or arg_film < 1:
+        print('Veuillez choisir un bon argument')
+        sys.exit()
+    else:
+        # Going to the film webpage
+        for u_soup in soup.find_all(class_="cover_infos_title"):
+            film_name = u_soup.find('a').get_text()
+            if film_name == film_list[arg_film - 1]:
+                film_link = f"{url}{u_soup.find('a').get('href')}"
+                break
+
+    r_film = requests.get(film_link)
+    s_film = BeautifulSoup(r_film.text, 'html.parser')
+
+    qualities.append(film_link)
+
+    # Getting all the qualities and printing them
+    if s_film.find(class_='otherversions'):
+        for link in s_film.find(class_='otherversions').find_all('a'):
+            qualities.append(f"{url}{link.get('href')}")
+
+    clear()
+    a = 1
+    for qualitie in qualities:
+        print(
+            f'{a}. Qualité : {Fore.MAGENTA + get_attrs(qualitie)[0] + Style.RESET_ALL} \n| Langue : {Fore.LIGHTYELLOW_EX + get_attrs(qualitie)[1] + Style.RESET_ALL} \n| Taille du fichier : {Fore.GREEN + get_attrs(qualitie)[2] + Style.RESET_ALL} \n| Url : {Fore.CYAN + get_attrs(qualitie)[3] + Style.RESET_ALL}')
+        a = a + 1
+
+    arg_qualitie = int(input('Veuillez rentrer le nombre de la qualité choisie : '))
+
+    if arg_qualitie > len(qualities) or arg_qualitie < 1:
+        print('Veuillez choisir un bon argument')
+        sys.exit()
+
+    else:
+        clear()
+        # Données du formulaire pour la request POST
+        headers_form = {
+            'cookie': 'AF=22123'
+        }
+        data_form = {
+            'dl_no_ssl': 'off',
+            'dlinline': 'off'
+        }
+
+        # Tries to bypass the url (BETA)
+        print('Résolution du captcha...', end=' ')
+        fichier_url = bypass(get_attrs(qualities[arg_qualitie - 1])[3])
+        print(Fore.GREEN+'Effectué.'+Style.RESET_ALL)
+
+        # Check si le fichier est delete
+        if BeautifulSoup(requests.get(fichier_url).text, 'html.parser').find(class_='bloc2'):
+            print(Fore.RED + 'Fichier supprimé' + Style.RESET_ALL)
+            sys.exit(0)
+
+        print('Obtention du lien de téléchargement...', end=' ')
+        # Request POST
+        r_form = requests.post(fichier_url, headers=headers_form, data=data_form)
+        soup_form = BeautifulSoup(r_form.text, 'html.parser')
+        button = soup_form.find(class_="ok btn-general btn-orange")
+
+        name = f"{film_name1} {get_attrs(qualities[arg_qualitie - 1])[0]}"
+
+        try:
+            # Trying to give the dl link (if no download in the last 2 hours)
+            dl_url = button['href']
+            print('Effectué.')
+            dl(dl_url, name)
+
+        # Print erreur si délai de dl
+        except Exception as e:
+            print(Fore.RED + ' \nBypass du lien requis.' + Style.RESET_ALL + '\nMise en route du bypasser...')
+            try:
+                dl(bypass_1fichier(fichier_url, proxies), name)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    main()
